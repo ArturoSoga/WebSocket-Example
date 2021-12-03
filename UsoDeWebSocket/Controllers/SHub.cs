347ea0ed8e1f7ddd1f5d515270889bb3b38bb4c3
Arturo <arturo.soga01@gmail.com>
2021-12-02 22:56:09 -0600

    Inicio de la implementacion

---
+++ UsoDeWebSocket/Controllers/SHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsoDeWebSocket.Controllers
+{
+    public class SHub : Hub
+    {
+        public Task SendMesssage(string user , string message,string tag)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", "Arturo", "Mensaje en tiempo real");
+        }
+    }
+}
---
+++ UsoDeWebSocket/Controllers/SocketHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsoDeWebSocket.Controllers
+{
+    public class SocketHub : ControllerBase
+    {
+
+        [Route("api/HUB")]
+        [HttpPost]
+        public string HUB(string message , string tag , string user)
+        {
+            var socket = new SHub();
+            socket.SendMesssage(message,tag,user);
+
+            return null;
+        }
+
+    }
+}
